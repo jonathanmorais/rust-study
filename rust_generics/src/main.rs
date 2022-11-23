8633feda9937f9c6685e5cbce70b8afba425d64a
jonathanmorais <jonathan.m.lucena@gmail.com>
2022-11-23 13:18:02 -0300

    generics

---
+++ rust_generics/src/main.rs
@@ -0,0 +1,62 @@
+#![allow(unused)]
+use std::io;
+use rand::Rng;
+use std::io::{Write, BufReader, BufRead, ErrorKind};
+use std::fs::File;
+use std::cmp::Ordering;
+
+// generics
+
+// A concrete type `A`.
+struct A;
+
+// In defining the type `Single`, the first use of `A` is not preceded by `<A>`.
+// Therefore, `Single` is a concrete type, and `A` is defined as above.
+struct Single(A);
+//            ^ Here is `Single`s first use of the type `A`.
+
+// Here, `<T>` precedes the first use of `T`, so `SingleGen` is a generic type.
+// Because the type parameter `T` is generic, it could be anything, including
+// the concrete type `A` defined at the top.
+struct SingleGen<T>(T);
+
+// traits
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+} 
+
+pub trait Summarize {
+    fn summarize(&self) -> String;
+}
+
+impl Summarize for Tweet {
+    fn summarize(&self) -> String {
+        format!("{}: {}", self.username, self.content)
+    }
+}
+
+fn main(){
+    let tweet = Tweet {
+        username: String::from("jonathan"),
+        content: String::from("every family are happy of the same way"),
+        reply: true,
+    };
+    
+    println!("{}", tweet.summarize());
+
+    // `Single` is concrete and explicitly takes `A`.
+    let _s = Single(A);
+
+    // Create a variable `_char` of type `SingleGen<char>`
+    // and give it the value `SingleGen('a')`.
+    // Here, `SingleGen` has a type parameter explicitly specified.
+    let _char: SingleGen<char> = SingleGen('a');
+
+    // `SingleGen` can also have a type parameter implicitly specified:
+    let _t    = SingleGen(A); // Uses `A` defined at the top.
+    let _i32  = SingleGen(6); // Uses `i32`.
+    let _char = SingleGen('a'); // Uses `char`.
+}
